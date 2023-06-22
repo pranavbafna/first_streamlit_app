e7daa11cbee8b1bface332a70e8172d73ab2c160
Pranav Bafna <pranav.bafna21@gmail.com>
2023-06-22 09:24:38 +0530

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','banana')
-streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add','banana')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
