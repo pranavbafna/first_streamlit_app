5e33cfacdd6fef102356a21fb3ab2151ab07e770
Pranav Bafna <pranav.bafna21@gmail.com>
2023-06-22 09:35:15 +0530

    Beautification

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,40 +2,41 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
+#Read table from storage bucket and set index to fruit
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+#Display Text
 streamlit.title('My Parents healthy Dinner')
-  
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#Multiselect Fruits and display in DF
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#User Input and get info from API Call
 streamlit.header("Fruityvice Fruit Advice!")
-
-
-
-#streamlit.text(fruityvice_response.json())
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+#Snowflake
 add_my_fruit = streamlit.text_input('What fruit would you like to add','banana')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
